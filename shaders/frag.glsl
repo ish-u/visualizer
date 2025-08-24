a88599545e2118517ee3dacfb2f1b3c6fe7553d1
Anmol Gupta <anmolgupta520@gmail.com>
2025-08-25 01:30:08 +0530

    minor impovement

--- shaders/frag.glsl
+++ shaders/frag.glsl
@@ -10,7 +10,7 @@ out vec4 outColor;
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     vec2 uv = fragCoord.xy / iResolution.xy;
-    float y = step(uv.y - (0.5 +  (0.5 *samples[int(uv.x * (512.0))])), 0.0f);
+    float y = 0.5 + step(uv.y - (0.5 +  (0.5 *samples[int(uv.x * (512.0))])), 0.0f);
     vec3 col = vec3(y * (0.5 + 0.5*cos(iTime+uv.xyx+vec3(0,2,4)))); 
     fragColor = vec4(col,1.0);
 }
